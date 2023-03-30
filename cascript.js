52e1d86ef523a71640c8ab378cc6a7be85144988
Karthikramachandran31 <126076732+Karthikramachandran31@users.noreply.github.com>
2023-03-30 19:57:03 +0530

    Add files via upload

---
+++ cascript.js
@@ -0,0 +1,16 @@
+function dis(x) {
+    if (document.getElementById("input").value == 0) {
+        document.getElementById("input").value = "";
+        document.getElementById("input").value = x;
+    } else {
+        document.getElementById("input").value += x;
+     }
+ }
+ function solve() {
+    var x = document.getElementById("input").value;
+    var y = eval(x);
+    document.getElementById("input").value = y;
+}
+function clean() {
+    document.getElementById("input").value = 0;
+}
